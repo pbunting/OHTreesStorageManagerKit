1178769efc2c88c122b18a8154921debcf891af4
100 Trees <100trees.com@gmail.com>
2016-04-24 18:39:52 -0700

    try adding watch kit support

---
+++ OHTreesStorageManagerKitWatch/OHTreesStorageManagerKitWatch.h
@@ -0,0 +1,19 @@
+//
+//  OHTreesStorageManagerKitWatch.h
+//  OHTreesStorageManagerKitWatch
+//
+//  Created by Paul Bunting on 4/24/16.
+//  Copyright © 2016 Paul Bunting. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for OHTreesStorageManagerKitWatch.
+FOUNDATION_EXPORT double OHTreesStorageManagerKitWatchVersionNumber;
+
+//! Project version string for OHTreesStorageManagerKitWatch.
+FOUNDATION_EXPORT const unsigned char OHTreesStorageManagerKitWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OHTreesStorageManagerKitWatch/PublicHeader.h>
+
+
